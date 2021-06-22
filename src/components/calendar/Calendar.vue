932f64a85c9eb66687b0f1f03048bb93d1d12050
Zuhaib Khan <xuhaib002@gmail.com>
2021-06-22 21:10:21 +0500

    embedded calendar

--- src/components/calendar/Calendar.vue
+++ src/components/calendar/Calendar.vue
@@ -1,10 +1,32 @@
 <template>
-  <div class="calendar">calendar</div>
+  <div class="calendar">
+    <v-date-picker
+      :value="null"
+      color="indigo"
+      v-model="range"
+      is-range
+      is-expanded
+      style="height: 100%"
+      show-weeknumbers
+    />
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      range: {
+        start: new Date(2021, 0, 17),
+        end: new Date(2021, 0, 20),
+      },
+    };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .calendar {
   border-radius: var(--border-radius-small);
-  padding: 1.5rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,8 +9,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import icons from '@/common/icons.js';
 library.add(...icons);
 
+// v-calendar libaray
+import VCalendar from 'v-calendar';
+
 const app = createApp(App);
 app.use(store);
 app.use(router);
+app.use(VCalendar, {});
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
